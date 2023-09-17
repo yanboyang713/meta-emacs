7d7d8a3d2851bedf8d01d87c34d60fe09bc1423c
Boyang Yan <yanboyang713@gmail.com>
2023-09-16 21:47:04 -0400

    add lisp

---
+++ lisp/init-straight-package.el
@@ -0,0 +1,25 @@
+;;; init-straight-package.el --- Set-up straight package manger -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; Straight package management
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq package-enable-at-startup nil)
+
+;; Use-package
+(straight-use-package 'use-package)
+
+(provide 'init-straight-package)
+;;; init-straight-package.el ends here
