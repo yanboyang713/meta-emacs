79aef06ca756a6250cdeaa252b576c5afdbe84fd
Boyang Yan <yanboyang713@gmail.com>
2023-09-16 20:55:59 -0400

    add early-init and gitignore

--- init.el
+++ init.el
@@ -1,3 +1,22 @@
+;;; init.el --- Load the full configuration -*- lexical-binding: t -*-
+
+(let ((minver "26.1"))
+  (when (version< emacs-version minver)
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+(when (version< emacs-version "27.1")
+  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
+
+;; Add user emacs dir = lisp
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+;; Adjust garbage collection thresholds during startup, and thereafter
+
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
 ;;;;;;;;;;;;;; 
 ;; STARTUP  ;;
 ;;;;;;;;;;;;;;
