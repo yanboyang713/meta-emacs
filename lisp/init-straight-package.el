8acf0d5b60a5b5f6923b1b7995cc4b7b784474a2
Boyang Yan <yanboyang713@gmail.com>
2026-07-06 21:25:50 -0400

    Adapt Emacs config for local setup

--- lisp/init-straight-package.el
+++ lisp/init-straight-package.el
@@ -20,6 +20,34 @@
 
 ;; Use-package
 (straight-use-package 'use-package)
+(straight-use-package 'seq)
+(straight-use-package 'compat)
+(straight-use-package 'transient)
+
+(defun yb/patch-transient-box-faces ()
+  "Patch Transient face specs that this Emacs build rejects in GUI frames."
+  (let ((transient-file
+         (expand-file-name "straight/repos/transient/lisp/transient.el"
+                           user-emacs-directory))
+        (transient-elc
+         (expand-file-name "straight/build/transient/transient.elc"
+                           user-emacs-directory)))
+    (when (file-readable-p transient-file)
+      (with-temp-buffer
+        (insert-file-contents transient-file)
+        (goto-char (point-min))
+        (when (search-forward ":line-width (-1 . -1)" nil t)
+          (goto-char (point-min))
+          (while (search-forward ":line-width (-1 . -1)" nil t)
+            (replace-match ":line-width -1" t t))
+          (write-region (point-min) (point-max) transient-file)
+          (when (file-exists-p transient-elc)
+            (delete-file transient-elc)))))))
+
+(yb/patch-transient-box-faces)
+(require 'seq)
+(require 'compat)
+(require 'transient)
 
 (provide 'init-straight-package)
 ;;; init-straight-package.el ends here
